d9f2c8b055b60b306e84495739cfcc251c6ceb9c
stephenprasetyachrismawan <stephenprasetyachrismawan27@gmail.com>
2025-05-07 10:51:55 +0700

    [fix] more satisfying hacked

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyAUZ8fcR7LKFPfMDUHsz3q8JuzbSaTJOq4",
+    authDomain: "bible-ppt-app.firebaseapp.com",
+    projectId: "bible-ppt-app",
+    storageBucket:"bible-ppt-app.firebasestorage.app",
+    messagingSenderId: "826124420823",
+    appId: "1:826124420823:web:fea4952ad1603b3aa31292",
+    measurementId:"G-JCZK9R74C5"
 };
 
 // Initialize Firebase
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAUZ8fcR7LKFPfMDUHsz3q8JuzbSaTJOq4",
+  authDomain: "bible-ppt-app.firebaseapp.com",
+  projectId: "bible-ppt-app",
+  storageBucket:"bible-ppt-app.firebasestorage.app",
+  messagingSenderId: "826124420823",
+  appId: "1:826124420823:web:fea4952ad1603b3aa31292"
 };
 
 // Initialize Firebase only if it hasn't been initialized already
